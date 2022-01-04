736c5e0ad16762085574e179947c86a4f905e1b3
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-01-04 17:03:47 -0600

    learn about string slicing

--- strings.py
+++ strings.py
@@ -20,4 +20,30 @@ print(parrot[3])
 print()
 print(parrot[-1])   # e = last character
 
+# Slicing = start stop step. Note the : otherwise it's an index
+print(parrot[0:6])  # Norweg
+print(parrot[3:5])  # we
+print(parrot[:9])   # Norwegian
+print(parrot[10:])  # Blue
+
+# Can also use negative numbers
+print(parrot[-4:-2])    # Bl
+
+# The step value
+print(parrot[0:6:2])    #Nre
+
+number = "9,223,372,354,875,607"
+print(number[1::4])
+
+# Slicing backwards
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+# [::-1] is a Python Idiom
+backwards = letters[::-1]
+print(backwards)
+
+
+
+
+
 
