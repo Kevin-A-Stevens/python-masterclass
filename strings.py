64336cdec50667eed8f85524b9835e895b3ff018
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-01-03 21:07:16 -0600

    learn about strings

---
+++ strings.py
@@ -0,0 +1,16 @@
+print("Today ios a good day to learn Python")
+print("Python is fun")
+print("Python's strings and easy to use")
+print("We can even use 'quotes' in strings")
+
+# string concatenation
+print("Hello" + " world")
+
+# setting variables
+greeting = "Hello"
+
+# getting input from user
+name = input("Please enter your name ")
+
+print(greeting + " " + name)
+
