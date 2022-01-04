1751a74e063664c51590bd4532132c976a35bef7
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-01-03 21:29:15 -0600

    learn about escape character

---
+++ escapechar.py
@@ -0,0 +1,27 @@
+# \n prints a new line
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+# \t prints a tab
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+# The \ can also be used to escape characters
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+
+# can also use """ and no need to use escape character
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+anotherSplitString = """This string has been
+split over
+several
+lines"""
+
+print(anotherSplitString)
+
+# include the \ character
+print("C:\\Users\\kstevens\\notes.txt") # preferable
+print(r"C:\Users:\kstevens\notes.txt")
+
+age = 24
+print(age)
