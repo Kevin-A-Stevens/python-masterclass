db4f47591a2500520ab400862ddcd1677e562659
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-01-10 05:44:58 -0600

    for loops

--- Basics3.py
+++ Basics3.py
@@ -45,3 +45,81 @@ is_user = True
 # The same with using and. It sees False and does not need to evaluate the second part
 if is_friend and is_user:
     print("Best friends forever")
+
+# Logical Operator
+print(4 > 5)
+print(4 < 5)
+print(4 == 5)
+print(1 >= 2)
+print(1 != 2)
+print( 3 <= 3)
+print(not(True)) # not gives the opposite
+
+print(True == 1)
+# print(True is 1) # checks if the location in memory is the same
+
+# for loops
+# iterable = something that is able to get looped over
+# Iterated = one by one checks each item in the collection
+for item in "Zero to Mastery":
+    print(item)
+
+for item in (1,2,3,4,5):
+    print(item)
+
+# nested for loops
+for item in (1,2,3,4,5):
+    for x in ["a", "b", "c"]:
+        print(item, x)
+
+user = {
+    "name": "Golem",
+    "age": 5006,
+    "can_swim": False
+}
+
+for item in user:
+    print(item) # prints keys of the user dictionary
+
+for item in user.items():
+    print(item) # prints keys and values of the user dictionary
+
+for item in user.values():
+    print(item) # prints values of the user dictionary
+
+for item in user.keys():
+    print(item) # prints keys of the user dictionary
+
+for key,value in user.items():
+    print(key, value)  # prints keys and values of the user dictionary
+
+# Counter = counts the items in our list and adds them together
+my_list = [1,2,3,4,5,6,7,8,9,10]
+counter = 0
+for item in my_list:
+    counter = counter + item
+print(counter)
+
+# range() function = produces a sequence of integers. (start, stop, step)
+for number in range(0, 50, 2):
+    print(number)  # prints 0 to 49
+
+for number in range(10, 0, -1):
+    print(number)  # prints 10 to 1
+
+# create 3 lists
+for i in range(3):
+    print(list(range(10)))
+
+# enumerate() = allows you to get the index number of each item
+for i,char in enumerate("Hello"):
+    print(i, char)
+
+for i,char in enumerate((1,2,3)):
+    print(i, char)
+
+for i,char in enumerate(list(range(50))):
+    if char == 25:
+        print(f"index of 25 is: {i}")
+
+
