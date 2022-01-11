3e6dd20c14231e5c032180eb15bfef187f180638
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-01-10 21:16:45 -0600

    scope

--- Basics5.py
+++ Basics5.py
@@ -15,3 +15,66 @@ print(super_func(1,2,3,4,5, num1=5, num2=10))
 # Rule: params, *args, default parameters, **kwargs
 # Usually you are using only 1 or 2 of these
 
+# Walrus Operator := Assigns values to variables as part of a larger expression
+a = "Hellooooooooooooo"
+if ((n := len(a)) > 10):
+    print(f"Too long {n} elements.")
+
+while ((n := len(a)) > 1):
+    print(n)
+    a = a[:-1]
+print(a)
+
+# Scope = What variables do I have access to
+# Global scope = anyone can use this variable anywhere
+
+a = 1
+def conf():
+    a = 5
+    return a
+print(a)  # 1
+print(conf())  # 5
+print(a)  # 1
+
+# Scope Rules
+"""
+1 - Start with local
+2 - Parent local?
+3 - Global
+4 - Built in Python functions
+"""
+
+# Global keyword
+"""
+total = 0
+def count():
+    global total
+    total += 1
+    return total
+count()
+count()
+print(count())
+"""
+
+# A better way is not using the global keyword
+total = 0
+def count(total):
+    total += 1
+    return total
+count(total)
+count(total)
+print(count(count(count(total))))
+
+# nonlocal keyword
+def outer():
+    x = "local"
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner", x)
+    inner()
+    print("outer", x)
+outer()
+
+x = float(2.8)
+print(x)
