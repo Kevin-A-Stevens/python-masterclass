01555ab8408e1448bc54204a4324866bf8aba9a4
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-01-31 22:38:36 -0600

    translator

--- translator.py
+++ translator.py
@@ -1,13 +1,15 @@
 from translate import Translator
 
+# https://pypi.org/project/translate/
+# pip3 install translate
 # en, ja, ko, pt, zh
-translator = Translator(to_lang="ja")
+translator = Translator(to_lang="es")
 
 try:
     with open("me.txt", mode="r") as my_file:
         text = my_file.read()
         translation = translator.translate(text)
-        with open("me-ja.txt", mode="w") as my_file2:
+        with open("me-es.txt", mode="w") as my_file2:
             my_file2.write(translation)
 except FileNotFoundError as e:
     print("Check your file path")
