3eaf089200652b055f6eba5ef96a5ed05abb0303
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-02-05 20:57:13 -0600

    sending emails

--- python_scripting/email_sender.py
+++ python_scripting/email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path  # use pathlib instead of os.path
 
+html = Template(Path("index.html").read_text())
 email = EmailMessage()
 email["from"] = "Kevin Stevens"
 email["to"] = "kevin.alan.stevens@gmail.com"
 email["subject"] = "Test from Python"
 
-email.set_content("I am a Python developer!")
+email.set_content(html.substitute({"name": "Kevin"}), "html")
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
     smtp.ehlo()
