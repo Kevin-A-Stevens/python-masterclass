dd3418a7c20963e17bdbcdfac3e3193559f5bbb8
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-02-06 13:10:00 -0600

    password checker complete

--- python_scripting/passwordcheck.py
+++ python_scripting/passwordcheck.py
@@ -1,6 +1,46 @@
 # python3 passwordcheck.py Password1 Password2 Password3
 import requests
+import hashlib
+import sys
 
-url = "https://api.pwnedpasswords.com/range/" + "CBFDA"
-res = requests.get(url)
-print(res)
+def request_api_data(query_char):
+    url = "https://api.pwnedpasswords.com/range/" + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f"Error fetching: {res.status_code}, check the API and try again")
+    return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    # Check password if it exists in API response
+    """
+    print(password.encode("utf-8"))  # b'123'
+    print(hashlib.sha1(password.encode("utf-8")))  # <sha1 HASH object @ 0x7f863138aad0>
+    print(hashlib.sha1(password.encode("utf-8")).hexdigest()) # 40bd001563085fc35165329ea1ff5c5ecbdbbeef
+    print(hashlib.sha1(password.encode("utf-8")).hexdigest().upper())  # 40BD001563085FC35165329EA1FF5C5ECBDBBEEF
+    """
+    sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char, tail)
+    return get_password_leaks_count(response, tail)
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f"{password} was found {count} times...you should probably change your password")
+        else:
+            print(f"{password} was not found. Carry on!")
+    return "done!"
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))  # sys.exit exits out of the file and returns done!
+
+# Improvement to be made: read passwords from a text file instead of the command line
